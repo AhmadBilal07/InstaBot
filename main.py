39465f22dbb74ea916c19f706654a81572209e38
AhmadBilal07 <ahmadbilal07@live.com>
2020-10-07 21:04:47 +0200

    instabot basic

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('helloWorld')
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import set_workspace
+import time
+
+#your login credentials
+insta_username='ahmadbilalaslam@gmail.com'
+insta_password='Parkinson@123'
+
+def job():
+  session = InstaPy(username=insta_username, password=insta_password,geckodriver_path="C:\\Users\\ahmad\\Downloads\\geckodriver.exe")
+  with smart_run(session):
+      session.follow_by_tags([ 'mahirakhan','fawadkhan'], amount=120)
+job()
